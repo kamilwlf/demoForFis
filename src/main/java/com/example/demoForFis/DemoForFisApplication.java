1c72ff5893e7449d9d142cb548786cd943724f39
kamilwlf <kamil4772@gmail.com>
2020-08-24 23:42:55 +0200

    main class

---
+++ src/main/java/com/example/demoForFis/DemoForFisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoForFis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForFisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForFisApplication.class, args);
+	}
+
+}
